4fd9f338d887ab38c12945c7a32eb94fb95f630b
heleomsft <heleo@microsoft.com>
2021-08-21 20:44:00 -0300

    Adding provider version.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,14 @@
 provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.46.0"
     features {}
 }
 
 terraform {
+    required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "terraform-azure-lab"
     storage_account_name = "satfazurelab"
